6f6e93710804dec480caf10da840e99129daa2d4
Nazir Azizi <nazirazizis123456@gmail.com>
2024-12-16 23:04:50 -0800

    Update ans.java

--- Remove Element - 27/ans.java
+++ Remove Element - 27/ans.java
@@ -2,16 +2,8 @@ class Solution {
     public int removeElement(int[] nums, int val) {
         int c = 0;
         for (int i = 0; i < nums.length; ++i){
-            if (nums[i] != val){
-                int j = i;
-                c++;
-                int value = nums[j];
-                while (j > 0 && nums[j - 1] == val){
-                    nums[j] = nums[j - 1];
-                    j--;
-                }
-                nums[j] = value;
-            }
+            if (nums[i] != val)
+                nums[c++] = nums[i];
         }
         return c;
     }
