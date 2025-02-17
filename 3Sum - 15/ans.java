1f1f7cb44992a82952300fb9ac40e33d8b6f4081
Nazir Azizi <nazirazizis123456@gmail.com>
2025-02-17 03:22:19 -0800

    Update ans.java

--- 3Sum - 15/ans.java
+++ 3Sum - 15/ans.java
@@ -1,16 +1,20 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> list = new ArrayList<>();
         for(int i = 0; i < nums.length - 2; ++i){
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
             int j = i + 1;
             int k = nums.length - 1;
             while (j < k){
                 int sum = nums[i] + nums[j] + nums[k];
                 if (sum == 0){
-                    set.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k])));
+                    list.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k])));
                     j++;
                     k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
                 }else if (sum < 0){
                     j++;
                 }else{
@@ -18,6 +22,6 @@ class Solution {
                 }
             }
         }
-        return new ArrayList<>(set);
+        return list;
     }
 }
