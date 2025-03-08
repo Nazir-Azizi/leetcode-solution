7cb02918567ff71afde8daf7db37e8948f469a9d
Nazir Azizi <nazirazizis123456@gmail.com>
2025-03-08 02:55:57 -0800

    Update ans.java

--- Maximum Average Subarray I - 643/ans.java
+++ Maximum Average Subarray I - 643/ans.java
@@ -4,20 +4,11 @@ class Solution {
         for(int i = 0; i < k; ++i)
             sum += nums[i];
         
-        int j = 0;
         int res = sum;
-        // System.out.println("first k: " + nums[k - 1]);
+        
         for(int i = k; i < nums.length; ++i){
-            // System.out.println("sum: " + sum);
-            // System.out.println("res: " + res);
-            if (nums[j] > 0)
-                sum -= nums[j];
-            else
-                sum += Math.abs(nums[j]);
-            sum += nums[i];
-            if (res < sum)
-                res = sum;
-            j++;
+            sum = sum - nums[i - k] + nums[i];
+            res = Math.max(res, sum);
         }
         return res / (double)k;
     }
